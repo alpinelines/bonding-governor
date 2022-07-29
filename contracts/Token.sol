ae559e5cef55124b14d55330cfc601f617cb4954
Josh <35016946+alpinelines@users.noreply.github.com>
2022-07-29 12:02:44 -0700

    init commit

---
+++ contracts/Token.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title Token
+contract Token is ERC20, ERC20Burnable, ERC20Votes, AccessControl {
+    string public version;
+
+    bytes32 public constant VAULT_ROLE = keccak256("VAULT_ROLE");
+
+    constructor() ERC20("Bonding Token", "BTX") ERC20Permit("Bonding Token") {
+        version = "0.2";
+    }
+
+    function setVault(address vault) public {
+        require(hasRole(VAULT_ROLE, msg.sender), "ACCESS_CONTROL: function requires ADMIN_ROLE");
+        grantRole(VAULT_ROLE, vault);
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(hasRole(VAULT_ROLE, msg.sender), "ACCESS_CONTROL: function requires ADMIN_ROLE");
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 value) external {
+        require(hasRole(VAULT_ROLE, msg.sender), "ACCESS_CONTROL: function requires ADMIN_ROLE");
+        _burn(from, value);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        require(hasRole(VAULT_ROLE, msg.sender), "ACCESS_CONTROL: function requires ADMIN_ROLE");
+        ERC20Votes._mint(to, amount);
+    }
+
+    function _burn(address from, uint256 value) internal override(ERC20, ERC20Votes) {
+        require(hasRole(VAULT_ROLE, msg.sender), "ACCESS_CONTROL: function requires ADMIN_ROLE");
+        ERC20Votes._burn(from, value);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+}
---
+++ contracts/Vault.sol
@@ -0,0 +1,81 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./BancorZeroFormula.sol";
+import "./IBondingCurveToken.sol";
+
+/// @title Vault
+/// @author Carl Farterson (@carlfarterson) && Chris Robison (@cbobrobison)
+contract Vault is BancorZeroFormula {
+
+    uint256 constant PRECISION = 10**18;
+
+    /// @dev Token issued by the bonding curve
+    IBondingCurveToken immutable token;
+    /// @dev Token used as collateral for minting/burning the Token issued by the bonding curve
+    IBondingCurveToken immutable collateral;
+
+    /// @dev The ratio of how much collateral "backs" the total marketcap of the Token (eg, creates the shape of the curve)
+    uint32 immutable connectorWeight;
+    /// @dev The intersecting price to mint or burn a Token when supply == PRECISION (eg, creates the slope of the curve)
+    uint256 immutable baseY;
+
+    /// @dev The amount of collateral "backing" the total marketcap of the Token
+    uint256 connectorBalance = 0;
+
+    constructor(
+        IBondingCurveToken _token,
+        IBondingCurveToken _collateral,
+        uint32 _connectorWeight,
+        uint256 _baseY
+    ) {
+        require(_connectorWeight <= 1000000 && _connectorWeight > 0);
+        token = _token;
+        collateral = _collateral;
+        connectorWeight = _connectorWeight;
+        baseY = _baseY;
+    }
+
+    function sell(
+        uint256 _collateralDeposited,
+        address _recipient
+    ) external returns (uint256 tokensReturned) {
+        uint256 supply = token.totalSupply();
+        if (supply > 0) {
+            tokensReturned = _calculatePurchaseReturn(
+                _collateralDeposited,
+                connectorWeight,
+                supply,
+                connectorBalance
+            );
+        } else {
+            tokensReturned = _calculatePurchaseReturnFromZero(
+                _collateralDeposited,
+                connectorWeight,
+                PRECISION,
+                baseY
+            );
+        }
+
+        connectorBalance += _collateralDeposited;
+        collateral.transferFrom(msg.sender, address(this), _collateralDeposited);
+        token.mint(_recipient, tokensReturned);
+    }
+
+    function buy(
+        uint256 _tokensBurned,
+        address _recipient
+    ) external returns (uint256 collateralReturned) {
+        uint256 supply = token.totalSupply();
+        collateralReturned = _calculateSaleReturn(
+            _tokensBurned,
+            connectorWeight,
+            supply,
+            connectorBalance
+        );
+
+        connectorBalance -= collateralReturned;
+        token.burn(msg.sender, _tokensBurned);
+        collateral.transferFrom(address(this), _recipient, collateralReturned);
+    }
+}
